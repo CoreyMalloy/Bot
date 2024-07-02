a84b6ec428c3218d83297745c856ba9ed55ef84b
Corey Malloy <Corey_8381@outlook.com>
2024-07-02 16:16:53 -0400

    init

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 //Require the correct classes
-const {Client, Events, GatewayInentBits} = require('discord.js');
-const {token} = require('./confg.json');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
 
 //creating a new client instance
-const client = new Client({ intents: [GatewayInentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, readyClient => {
   console.log('Ready! Logged in as ${readyClient.use.tag}');
 })
 
+client.commands = new Collection();
+
 //Using the token (password) to log into discord
 client.login(token);
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
